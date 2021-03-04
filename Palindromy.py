6ea2687b145949d59c4a35b0b8ea7236fa4630b3
Sylwia Zielińska <sylwiazielinska@yahoo.com>
2021-03-04 01:32:24 +0100

    Add comments

--- Palindromy.py
+++ Palindromy.py
@@ -1,6 +1,12 @@
 def palindrom(text):
+    '''
+    Funcja sprawdza czy słowo podane jako argument
+    pisane od lwewj do prawej
+    i od prawej do lewej brzmi jest tożsame.
+    
+    '''
     return text == text[::-1]
 if palindrom('inka'):
     print('yes')
 else:
-    print('dupa')
+    print('no')
